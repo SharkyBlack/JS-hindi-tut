c62fb9d52575db8beb5f06a4dede8fef3f4acc8c
Purab Gupta <121637308+SharkyBlack@users.noreply.github.com>
2025-04-10 06:53:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = "33"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log();
+//"33" => 33
+// "33abc" => NaN
+//true => 1; false =>0;
+
+let num = 33
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
+
