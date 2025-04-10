6c4ce9b2630ac10000972ddbf84a5e45dab4e640
Purab Gupta <121637308+SharkyBlack@users.noreply.github.com>
2025-04-10 07:05:52 +0000

    another

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,13 +1,29 @@
-let score = "33"
-console.log(typeof(score));
-let valueInNumber = Number(score)
-console.log();
-//"33" => 33
-// "33abc" => NaN
-//true => 1; false =>0;
+// let score = "33"
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log();
+// //"33" => 33
+// // "33abc" => NaN
+// //true => 1; false =>0;
 
-let num = 33
-let stringNum = String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num = 33
+// let stringNum = String(num)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+// ######## Operations #######
+
+let value =3
+let negvalue = -value
+// console.log(negvalue)
+
+let str1 = "hello"
+let str2 = " purab"
+let str3 = str1+ str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log('1'+2+2)
+// console.log(1+2+"2")
+console.log(+true)
+console.log(+"")
 
