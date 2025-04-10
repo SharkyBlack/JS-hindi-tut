d14769dee656589b01cd1857206c06eedb866163
Purab Gupta <121637308+SharkyBlack@users.noreply.github.com>
2025-04-10 13:38:34 +0000

    arrayintro

---
+++ 02_array/01_arraysintro.js
@@ -0,0 +1,20 @@
+const myArr = [0,1,2,3,4,5]
+console.log(myArr[3])
+
+//arrau ,metjpd
+
+// myArr.push(3)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr.includes(4))
+// console.log(myArr.indexOf(9))
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B ",myArr);
