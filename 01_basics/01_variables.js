9062ed42495e4cbf9fdf9c17e67a5f0d2dac73e6
Purab Gupta <121637308+SharkyBlack@users.noreply.github.com>
2025-04-10 06:38:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=14554
+let accountEmail="purabgupta2003@gmail.com"
+var accPassword = "12345"
+accountCity="jaipur"
+let accountState;
+// accountId = 2
+accountEmail="osfdoha#agh.com"
+accPassword="3455"
+accountCity="skfjs"
+console.log(accountId);
+console.table([accountId,accountEmail,accPassword,accountCity.accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+// alert('hello');//we are using nodejs not browser
+let name="purab"
+let age=18
+let isLoggedIn=false
+
+
+//number => 2 to power 53
+// bigint
+// string=> ""
+// boolean => true|false
+// null => standalone value
+// undefined =>value not defined yet
+// symbol => uniqueness find krne ke liye
+
+
+//object
+
+console.log(typeof null);//object
